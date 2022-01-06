3258f0e35cbfdc2ab3427edabe937d2f266b3e1a
Ana Beatriz <anabiawalker@gmail.com>
2022-01-06 17:32:38 -0300

    Adicionando integração com backend

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
+import { getSummary } from "./dashboardActions";
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
 import ValueBox from "../common/widget/valueBox";
 import Row from "../common/layout/row";
 
 class Dashboard extends Component {
+
+    //Método que renderiza a função
+    componentWillMount(){
+        this.props.getSummary()
+    }
+
     render(){
         const { credit, debt } = this.props.summary
         return (
@@ -18,7 +26,7 @@ class Dashboard extends Component {
 
                         <ValueBox cols="12 4" color="red" icon="credit-card" value={`R$ ${debt}`} text="Total de Débitos"/>
 
-                        <ValueBox cols="12 4" color="blue" icon="money" value={`R$ ${ credit - debt}`} text="Valor Consolidado"/>
+                        <ValueBox cols="12 4" color="blue" icon="money" value={`R$ ${credit - debt}`} text="Valor Consolidado"/>
                     </Row>
                 </Content>
             </div>
@@ -27,4 +35,6 @@ class Dashboard extends Component {
 }
 const mapStateToProps = state => ({summary: state.dashboard.summary})
 
-export default connect(mapStateToProps)(Dashboard)
+const mapDispatchToProps = dispatch => bindActionCreators({getSummary}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+
+import promise from 'redux-promise'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducers'
 
-const store = createStore(reducers)
+//Durante a criação da store, é aplicado um middleware que irá aguardar a resolução da promise
+const store = applyMiddleware(thunk, promise)(createStore)(reducers)
 
 ReactDOM.render(
     <Provider store={store}>
