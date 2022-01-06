48161ac14e170a2ef63ce4fc6ae4e80d5ec80974
Ana Beatriz <anabiawalker@gmail.com>
2022-01-06 14:40:15 -0300

    Adicionando footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2017 by
+            <a href="#"> Ana Beatriz da Conceição Walker</a>
+        </strong>
+    </footer>
+)
