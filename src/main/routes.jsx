62d450ddf212afa4efe7a2889bc757b83606088c
Ana Beatriz <anabiawalker@gmail.com>
2022-01-07 11:27:06 -0300

    Criando dashboard SEM Redux

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-import Dashboard from '../dashboard/dashboard'
+import Dashboard2 from '../dashboardReact/dashboard2'
+//import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard}/>
+        <Route path='/' component={Dashboard2}/>
+        {/*<Route path='/' component={Dashboard}/>*/}
         <Route path='/billingCycles' component={BillingCycle}/>
         <Redirect from='*' to='/'></Redirect>
     </Router>
