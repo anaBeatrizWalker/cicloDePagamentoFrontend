37d6fc70fb5a1422d9be2361bd3b0b57f38e8914
Ana Beatriz <anabiawalker@gmail.com>
2022-01-07 13:31:33 -0300

    Adicionando TabContent

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -6,6 +6,7 @@ import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
 import TabHeader from "../common/tab/tabHeader";
+import TabContent from "../common/tab/tabContent";
 
 class BillingCycle extends Component{
     render(){
@@ -25,7 +26,21 @@ class BillingCycle extends Component{
                             <TabHeader target='tabDelete' icon='trash-o' label='Exlcuir'/>
                         </TabsHeader>
                         <TabsContent>
+                            <TabContent id='tabList'>
+                                <h1>Lista</h1>
+                            </TabContent>
 
+                            <TabContent id='tabCreate'>
+                                <h1>Incluir</h1>
+                            </TabContent>
+
+                            <TabContent id='tabUpdate'>
+                                <h1>Alterar</h1>
+                            </TabContent>
+
+                            <TabContent id='tabDelete'>
+                                <h1>Excluir</h1>
+                            </TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
