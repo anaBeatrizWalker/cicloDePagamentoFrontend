3d014471483a81d397655c69586a1daa2436afe9
Ana Beatriz <anabiawalker@gmail.com>
2022-01-10 17:45:22 -0300

    Refatorando navegação com React-Router

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
-import App from './main/app'
+import Routes from './main/routes'
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
@@ -16,9 +16,10 @@ const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
 //Durante a criação da store, é aplicado um middleware que irá aguardar a resolução da promise
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 
+//Quem carrega app agora é Routes
 ReactDOM.render(
     <Provider store={store}>
-        <App/>
+        <Routes/>
     </Provider>
     , document.getElementById('app')
 )
--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
-import Dashboard2 from '../dashboardReact/dashboard2'
-//import Dashboard from '../dashboard/dashboard'
+import App from './app'
+//import Dashboard2 from '../dashboardReact/dashboard2'
+import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard2}/>
-        {/*<Route path='/' component={Dashboard}/>*/}
-        <Route path='/billingCycles' component={BillingCycle}/>
+        {/*<Route path='/' component={Dashboard2}/>*/}
+        
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashboard}></IndexRoute>
+            <Route path='billingCycles' component={BillingCycle}/>
+        </Route>
+        
         <Redirect from='*' to='/'></Redirect>
     </Router>
 )
