1e43a210720cd631fb56e5a3b888062c12a3359b
Ana Beatriz <anabiawalker@gmail.com>
2022-01-10 17:34:35 -0300

    Corrigindo responsividade do menu

--- src/common/template/menuTree.jsx
+++ src/common/template/menuTree.jsx
@@ -4,7 +4,7 @@ export default props => (
     /*item que tem subitens internamente*/
     <li className="treeview"> 
         <a href>
-            <i className={`fa fa-${props.icon}`}></i> {props.label}
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
             {/*se tá fechado o menu vai pra direita*/}
             <i className='fa fa-angle-left pull-right'></i>
         </a>
