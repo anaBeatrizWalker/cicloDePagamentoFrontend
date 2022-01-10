5326c5b4f81dcd0d9b3ba204dc7f6e91b6286601
Ana Beatriz <anabiawalker@gmail.com>
2022-01-10 17:28:20 -0300

    Adicionando resumo de créditos e débitos de um ciclo de pagamento

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -6,11 +6,26 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'; //método resp
 import { init } from "./billingCycleActions";
 import labelAndInput from "../common/form/labelAndInput";
 import ItemList from "./itemList";
+import Summary from "./summary";
 
 class BillingCycleForm extends Component{
+
+    calculateSummary(){
+        //acumulador e valor atual
+        const sum = (t, v) => t + v 
+        return {
+            //recupera todos os valores de créditos convertendo a númérico, senão é 0, e retorna um único valor pra soma de créditos
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum), 
+            sumOfDebits: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
     render(){
         //Processa o form da forma correta
         const { handleSubmit, readOnly, credits, debts } = this.props
+
+        const { sumOfCredits,  sumOfDebits} = this.calculateSummary()
+
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className="box-body">
@@ -37,6 +52,8 @@ class BillingCycleForm extends Component{
                         cols='12 4' 
                         placeholder='Informe o ano'>
                     </Field>
+
+                    <Summary credit={sumOfCredits} debt={sumOfDebits}/>
 
                     <ItemList cols='12 6' 
                         list={credits} 
